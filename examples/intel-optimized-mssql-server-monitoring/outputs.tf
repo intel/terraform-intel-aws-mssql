e699ececf48a2e571e28ec9f27fe3a54b4a1cd7e
Rajiv Mandal <rajiv.mandal@intel.com>
2023-01-20 10:08:42 -0800

    changed mysql references to mssql

--- examples/intel-optimized-mssql-server-monitoring/outputs.tf
+++ examples/intel-optimized-mssql-server-monitoring/outputs.tf
@@ -1,36 +1,36 @@
-output "mysql_address" {
-  description = "MySQL instance hostname"
+output "MSSQL_address" {
+  description = "MSSQL instance hostname"
   value       = module.optimized-mssql-server.db_hostname
 }
 
-output "mysql_port" {
-  description = "MySQL instance port"
+output "MSSQL_port" {
+  description = "MSSQL instance port"
   value       = module.optimized-mssql-server.db_port
 }
 
-output "mysql_username" {
-  description = "MySQL instance root username"
+output "MSSQL_username" {
+  description = "MSSQL instance root username"
   value       = module.optimized-mssql-server.db_username
   sensitive   = true
 }
 
-output "mysql_password" {
-  description = "MySQL instance master password."
+output "MSSQL_password" {
+  description = "MSSQL instance master password."
   value       = module.optimized-mssql-server.db_password
   sensitive   = true
 }
 
-output "mysql_endpoint" {
+output "MSSQL_endpoint" {
   value       = module.optimized-mssql-server.db_endpoint
-  description = "Connection endpoint for the MySQL instance that has been created"
+  description = "Connection endpoint for the MSSQL instance that has been created"
 }
 
-output "mysql_engine" {
+output "MSSQL_engine" {
   value       = module.optimized-mssql-server.db_engine
   description = "Database instance engine that was configured."
 }
 
-output "mysql_status" {
+output "MSSQL_status" {
   description = "Status of the database instance that was created."
   value       = module.optimized-mssql-server.db_status
 }
@@ -40,37 +40,37 @@ output "instance_class" {
   value       = module.optimized-mssql-server.instance_class
 }
 
-output "mysql_allocated_storage" {
+output "MSSQL_allocated_storage" {
   description = "Storage that was allocated to the instance when it configured."
   value       = module.optimized-mssql-server.db_allocated_storage
 }
 
-output "mysql_max_allocated_storage" {
+output "MSSQL_max_allocated_storage" {
   description = "Maximum storage allocation that is configured on the database instance."
   value       = module.optimized-mssql-server.db_max_allocated_storage
 }
 
-output "mysql_arn" {
+output "MSSQL_arn" {
   description = "ARN of the database instance."
   value       = module.optimized-mssql-server.db_arn
 }
 
-output "mysql_kms_key_id" {
+output "MSSQL_kms_key_id" {
   description = "KMS key that is configured on the database instance if storage encryption is enabled."
   value       = module.optimized-mssql-server.db_kms_key_id
 }
 
-output "mysql_backup_window" {
+output "MSSQL_backup_window" {
   description = "Configured backup window for the database instance."
   value       = module.optimized-mssql-server.db_backup_window
 }
 
-output "mysql_maintenance_window" {
+output "MSSQL_maintenance_window" {
   description = "Maintainence window for the database instance."
   value       = module.optimized-mssql-server.db_maintenance_window
 }
 
-output "mysql_db_name" {
+output "MSSQL_db_name" {
   description = "Name of the database that was created (if specified) during instance creation."
   value       = module.optimized-mssql-server.db_name
 }
